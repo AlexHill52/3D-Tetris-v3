977d22f697f594bfd8689a74f94b32096acab43f
AlexHill52 <157166615+AlexHill52@users.noreply.github.com>
2026-05-05 04:40:23 -0600

    Make grid able to change size

--- Assets/Scripts/Playfield.cs
+++ Assets/Scripts/Playfield.cs
@@ -20,6 +20,69 @@ public class Playfield : MonoBehaviour
             bottomPlane.transform.position = new Vector3(transform.position.x + (float)gridSizeX/2,
                                                             transform.position.y,
                                                             transform.position.z + (float)gridSizeZ/2);
+
+            //Retile
+            bottomPlane.GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(gridSizeX, gridSizeZ);
+        }
+
+        if(N != null)
+        {
+            //Resize plane
+            Vector3 scaler = new Vector3((float)gridSizeX/10, 1, (float)gridSizeY/10);
+            N.transform.localScale = scaler;
+
+            //Reposition bottom plane
+            N.transform.position = new Vector3(transform.position.x + (float)gridSizeX/2,
+                                                transform.position.y + (float)gridSizeY/2,
+                                                transform.position.z + (float)gridSizeZ);
+
+            //Retile
+            N.GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(gridSizeX, gridSizeY);
+        }
+
+        if(S != null)
+        {
+            //Resize plane
+            Vector3 scaler = new Vector3((float)gridSizeX/10, 1, (float)gridSizeY/10);
+            S.transform.localScale = scaler;
+
+            //Reposition bottom plane
+            S.transform.position = new Vector3(transform.position.x + (float)gridSizeX/2,
+                                                transform.position.y + (float)gridSizeY/2,
+                                                transform.position.z);
+
+            //Retile
+            //S.GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(gridSizeX, gridSizeY);
+        }
+
+        if(E != null)
+        {
+            //Resize plane
+            Vector3 scaler = new Vector3((float)gridSizeZ/10, 1, (float)gridSizeY/10);
+            E.transform.localScale = scaler;
+
+            //Reposition bottom plane
+            E.transform.position = new Vector3(transform.position.x + (float)gridSizeX,
+                                                transform.position.y + (float)gridSizeY/2,
+                                                transform.position.z + (float)gridSizeZ/2);
+
+            //Retile
+            E.GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(gridSizeZ, gridSizeY);
+        }
+
+        if(W != null)
+        {
+            //Resize plane
+            Vector3 scaler = new Vector3((float)gridSizeZ/10, 1, (float)gridSizeY/10);
+            W.transform.localScale = scaler;
+
+            //Reposition bottom plane
+            W.transform.position = new Vector3(transform.position.x,
+                                                transform.position.y + (float)gridSizeY/2,
+                                                transform.position.z + (float)gridSizeZ/2);
+
+            //Retile
+            //W.GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(gridSizeZ, gridSizeY);
         }
     }
 }
