03d196c4a8251deae1a4256d04141d6766f568c1
AlexHill52 <157166615+AlexHill52@users.noreply.github.com>
2026-05-09 22:10:19 -0600

    Add piece movement and rotation with arrow keys

--- Assets/Scripts/TetrisPiece.cs
+++ Assets/Scripts/TetrisPiece.cs
@@ -34,8 +34,70 @@ public class TetrisPiece : MonoBehaviour
 
             prevTime = Time.time;
         }
+
+        //Movement inputs
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SetMoveInput(Vector3.left);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SetMoveInput(Vector3.right);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SetMoveInput(Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SetMoveInput(Vector3.back);
+        }
+
+        //Rotation inputs
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SetRotationInput(new Vector3(0,0,90));
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SetRotationInput(new Vector3(0,0,-90));
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SetRotationInput(new Vector3(90,0,0));
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SetRotationInput(new Vector3(-90,0,0));
+        }
     }
 
+    public void SetMoveInput(Vector3 direction)
+    {
+        transform.position += direction;
+        if (!CheckValidMove())
+        {
+            transform.position -= direction;
+        }
+        else
+        {
+            Playfield.instance.UpdatePlayfield(this);
+        }
+    }
+
+    public void SetRotationInput(Vector3 rotation)
+    {
+        transform.Rotate(rotation, Space.World);
+        if (!CheckValidMove())
+        {
+            transform.Rotate(-rotation, Space.World);
+        }
+        else
+        {
+            Playfield.instance.UpdatePlayfield(this);
+        }
+    }
+    
     bool CheckValidMove()
     {
         foreach (Transform child in transform)
