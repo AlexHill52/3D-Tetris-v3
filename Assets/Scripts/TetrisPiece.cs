3769e6f5c8a378ecea80dfc3aa15faa39b707e82
AlexHill52 <157166615+AlexHill52@users.noreply.github.com>
2026-05-11 20:12:17 -0600

    Fixed inputs and disabled locomotion

--- Assets/Scripts/TetrisPiece.cs
+++ Assets/Scripts/TetrisPiece.cs
@@ -4,6 +4,8 @@ public class TetrisPiece : MonoBehaviour
 {
     float prevTime;
     float fallTime = 1f;
+     public float inputCooldown = 0.15f;
+     private float lastInputTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,23 +38,28 @@ public class TetrisPiece : MonoBehaviour
             prevTime = Time.time;
         }
 
+        if (Time.time - lastInputTime < inputCooldown)
+        {
+            return;
+        }
+
         //right joystick
         Vector2 rightHandInput = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
 
         //Movement inputs
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || rightHandInput.x < -0.8f)
+        if (Input.GetKey(KeyCode.LeftArrow) || rightHandInput.x < -0.8f)
         {
             SetMoveInput(Vector3.left);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) || rightHandInput.x > 0.8f)
+        if (Input.GetKey(KeyCode.RightArrow) || rightHandInput.x > 0.8f)
         {
             SetMoveInput(Vector3.right);
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow) || rightHandInput.y > 0.8f)
+        if (Input.GetKey(KeyCode.UpArrow) || rightHandInput.y > 0.8f)
         {
             SetMoveInput(Vector3.forward);
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow) || rightHandInput.y < -0.8f)
+        if (Input.GetKey(KeyCode.DownArrow) || rightHandInput.y < -0.8f)
         {
             SetMoveInput(Vector3.back);
         }
@@ -61,25 +68,25 @@ public class TetrisPiece : MonoBehaviour
         Vector2 leftHandInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
 
         //Rotation inputs
-        if (Input.GetKeyDown(KeyCode.A) || leftHandInput.x < -0.8f)
+        if (Input.GetKey(KeyCode.A) || leftHandInput.x < -0.8f)
         {
             SetRotationInput(new Vector3(0,0,90));
         }
-        if (Input.GetKeyDown(KeyCode.D) || leftHandInput.x > 0.8f)
+        if (Input.GetKey(KeyCode.D) || leftHandInput.x > 0.8f)
         {
             SetRotationInput(new Vector3(0,0,-90));
         }
-        if (Input.GetKeyDown(KeyCode.W) || leftHandInput.y > 0.8f)
+        if (Input.GetKey(KeyCode.W) || leftHandInput.y > 0.8f)
         {
             SetRotationInput(new Vector3(90,0,0));
         }
-        if (Input.GetKeyDown(KeyCode.S) || leftHandInput.y < -0.8f)
+        if (Input.GetKey(KeyCode.S) || leftHandInput.y < -0.8f)
         {
             SetRotationInput(new Vector3(-90,0,0));
         }
 
         //Hard-drop input
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || OVRInput.GetDown(OVRInput.Button.Three))
         {
             fallTime = 0.01f;
         }
@@ -96,6 +103,7 @@ public class TetrisPiece : MonoBehaviour
         {
             Playfield.instance.UpdatePlayfield(this);
         }
+        lastInputTime = Time.time;
     }
 
     public void SetRotationInput(Vector3 rotation)
@@ -109,6 +117,7 @@ public class TetrisPiece : MonoBehaviour
         {
             Playfield.instance.UpdatePlayfield(this);
         }
+        lastInputTime = Time.time;
     }
     
 
